55aa3dceed2f0734420ae7d06b0212866facd957
Delta9k8b <keaton.brewster@gmail.com>
2021-01-10 21:34:41 -0600

    added call to get min/max temperature for the current day. also fixed an issue will getting the uv index from a call

--- Main/script.js
+++ Main/script.js
@@ -25,24 +25,41 @@ $(document).ready(function () {
                 var card = $("<div>").attr({ "class": "card", "id": "current-card" }),
                     cardBody = $("<div>").addClass('card-body'),
                     title = $("<h3>").addClass('card-title').text(object.name + ' '),
-                    time = $("<p>").addClass('float-right').text(dayjs().format('MM-DD-YYYY')),
+                    time = $("<p>").addClass('float-right').text(dayjs().format('M-DD-YYYY')),
                     icon = $("<img>").attr('src', 'http://openweathermap.org/img/wn/' + object.weather[0].icon + ".png"),
+                    divider = $("<div>").addClass('dropdown-divider'),
                     conditions = $("<p>").addClass('card-text').text('Current conditions: ' + object.weather[0].description),
-                    temp = $("<p>").addClass('card-text').text('Temperature: ' + object.main.temp + '°f'),
+                    temp = $("<p>").attr({ 'class': 'card-text', 'id': 'current-temp' }).text('Temperature: ' + object.main.temp + '°f'),
                     humidity = $("<p>").addClass('card-text').text('Humidity: ' + object.main.humidity + '%'),
                     windSpeed = $("<p>").addClass('card-text').text('Wind speed: ' + object.wind.speed + 'mph');
 
                 title.append(icon, time);
-                cardBody.append(title, conditions, temp, humidity, windSpeed);
+                cardBody.append(title, divider, conditions, temp, humidity, windSpeed);
                 card.append(cardBody);
                 $("#current-div").append(card);
 
                 getForecast(searchValue);
 
                 // take lat and lon from the object, and send it to get the UV index (which requires a separate api call for some reason)
-                let lon = object.coord.lon;
-                let lat = object.coord.lat;
+                let lon = object.coord.lon,
+                    lat = object.coord.lat;
                 getUVIndex(lat, lon);
+
+                // get high/low for the current day
+                $.ajax({
+                    method: 'GET',
+                    url: "https://api.openweathermap.org/data/2.5/onecall?units=imperial&lat=" + lat + "&lon=" + lon + "&appid=" + myAPIkey,
+                    dataType: "json",
+                    success: function (object) {
+                        var high = $("<p>").addClass('card-text high').text('High: ' + object.daily[0].temp.max + '°f'),
+                            low = $("<p>").addClass('card-text low').text('Low: ' + object.daily[0].temp.min + '°f');
+
+                        $("#current-temp").append(high, low);
+                    },
+                    error: function (error) {
+                        console.log(error);
+                    }
+                });
             },
             error: function (error) {
                 console.log(error);
@@ -60,18 +77,19 @@ $(document).ready(function () {
                 for (let i = 0; i < object.list.length; i++) {
                     var hour = object.list[i];
                     if (hour.dt_txt.indexOf("15:00:00") !== -1) {
-                        var card = $("<div>").attr({'class':'card col-2','id':'forecast-day'}),
-                            cardBody = $("<div>").addClass('card-body');
-                            title = $("<p>").addClass('card-title').text(dayjs(hour.dt_txt).format('MM-DD')),
-                            conditions = $("<p>").addClass('card-text').text('conditions: ' + hour.weather[0].description),
+                        var card = $("<div>").attr({ 'class': 'card col-2', 'id': 'forecast-day' }),
+                            cardBody = $("<div>").addClass('card-body'),
+                            title = $("<p>").addClass('card-title').text(dayjs(hour.dt_txt).format('M-DD')),
                             icon = $("<img>").attr('src', 'http://openweathermap.org/img/wn/' + hour.weather[0].icon + ".png"),
+                            divider = $("<div>").addClass('dropdown-divider'),
+                            conditions = $("<p>").addClass('card-text').text('Conditions: ' + hour.weather[0].main),
                             temp = $("<p>").addClass('card-text').text('Temp: ' + hour.main.temp + '°f'),
                             humidity = $("<p>").addClass('card-text').text('Humidity: ' + hour.main.humidity + '%');
 
-                            title.append(icon);
-                            cardBody.append(title, conditions, temp, humidity);
-                            card.append(cardBody);
-                            $("#forecast-div").append(card);
+                        title.append(icon);
+                        cardBody.append(title, divider, conditions, temp, humidity);
+                        card.append(cardBody);
+                        $("#forecast-div").append(card);
                     }
                 }
 
@@ -86,10 +104,10 @@ $(document).ready(function () {
         // get uv index function
         $.ajax({
             type: "GET",
-            url: "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&appid=" + myAPIkey,
+            url: "https://api.openweathermap.org/data/2.5/onecall?units=imperial&lat=" + lat + "&lon=" + lon + "&appid=" + myAPIkey,
             dataType: "json",
             success: function (object) {
-                var uvindex = object.current.uvi,
+                var uvindex = object.daily[0].uvi,
                     uvEle = $("<p>").attr('class', 'card-text').text('UV Index: '),
                     uvP = $("<span>").text(uvindex);
                 if (uvindex < 3) { uvP.addClass('good-uvi') }
@@ -115,16 +133,9 @@ $(document).ready(function () {
     }
 
     // set up handler for when you click list item 
-    $("#history li").on("click", function(){
+    $("#history li").on("click", function () {
         search($(this).text());
     });
-
-    // set up initial time
-    $("#time").text(dayjs().format('dddd, MMMM D, h:mm a'))
-    // then update every 10 seconds
-    setInterval(() => {
-        $("#time").text(dayjs().format('dddd, MMMM D, h:mm a'))
-    }, 10000);
 
     // Assign the main function to the search button
     $("#search-button").on("click", function (event) {
